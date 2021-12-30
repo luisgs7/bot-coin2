1358563db2ede3038c90211e378b57feb03c7538
Luís Gustavo <54151493+luisgs7@users.noreply.github.com>
2021-12-30 00:17:48 +0000

    Update functions app.py

--- price/app.py
+++ price/app.py
@@ -5,17 +5,18 @@ def price_dolar():
   data = requests.get("https://economia.awesomeapi.com.br/json/last/USD-BRL").json()
   value = float(data["USDBRL"]["high"])
   date_time = data["USDBRL"]["create_date"]
-  value = round(value, 3)
+  value = format(value, '.2f')
   return (value, date_time)
 
 def price_btc():
   data = requests.get("https://economia.awesomeapi.com.br/json/last/BTC-BRL").json()
-  value = float(data["BTCBRL"]["bid"])
+  value = float(data["BTCBRL"]["bid"].replace('.',''))
+  value = format(value, '.2f')
   date_time = data["BTCBRL"]["create_date"]
-  value = round(value, 3)
   return (value, date_time)
 
 def price_eth():
   data = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=ETHBRL").json()
   value = float(data["price"])
+  value = format(value, '.2f')
   return value
