4292f4e3667d26b136765bb772f75b9ff47724c4
Luís Gustavo <luisgsilva270@gmail.com>
2022-02-04 13:49:44 +0000

    update: rename message convert_value

--- bot/messages.py
+++ bot/messages.py
@@ -4,16 +4,9 @@ menu_message = f'''Olá bem vindo ao BotCoin, digite a opção desejada:{os.line
       1 - Cotação do Dólar Americano {os.linesep}
       2 - Cotação do Bitcoin {os.linesep}
       3 - Cotação do Euro {os.linesep}
-      4 - Converter Valores {os.linesep}   
+      4 - Converter Valores (Em desenvolvimento) {os.linesep}    
       '''
 
 menu_help = f'Gostaria de acessar o menu? Digite "menu"'
 
-convert_value = f''' Escolha a opção desejada para realizar a conversão: {os.linesep}
-    1 - Converter de Dólar    -> Real
-    2 - Converter de Euro     -> Real
-    3 - Converter de Bitcoin -> Real{os.linesep}  
-    4 - Converter de Real     -> Dólar
-    5 - Converter de Real     -> Euro
-    6 - Converter de Real     -> Bitcoin            
-'''
+convert_value = f'Esta funcionalidade ainda está sendo desenvolvida :) !'
